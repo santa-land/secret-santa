c6c46e7e1a4dfbae33cc4e8882e6f989fa88ce26
Alireza Saberi <saberi.ar.eng@gmail.com>
2017-02-22 16:44:31 -0500

    feat: MongoLab is set up for the project
    Via config file, app should be able to conccet to mongolab and make basic
    stuffs.

--- server.js
+++ server.js
@@ -2,19 +2,29 @@
 * @Author: Ali
 * @Date:   2017-02-22 11:00:40
 * @Last Modified by:   Ali
-* @Last Modified time: 2017-02-22 14:09:04
+* @Last Modified time: 2017-02-22 16:32:54
 */
 
 /******** Requiring libraries ********/
 var express = require('express');
 var bodyParser= require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
 
 
 /******** Setting ********/
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', 3000);
-
+var config = require('./config/config.js');
+var db;
+MongoClient.connect( config.mongoLaClient, (err, database) => {
+    if (err) return console.log(err);
+    db = database;
+    app.listen(app.get('port'), function(){
+        console.log('Server is listening on port ' + app.get('port') + '. Press CTRL-C to terminate.');
+    });
+});
 
 /******** Routing ********/
 app.get('/', (req, res) => {
@@ -22,10 +32,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    console.log('Hellooooooooooooooooo!');
-    console.log(req.body);
+    req.body.match = '';
+    db.collection('gifters').save(req.body, (err, result) => {
+        if (err) return console.log(err);
+        console.log('saved to database');
+        res.redirect('/');
+    });
+
 });
 
-app.listen(app.get('port'), function(){
-    console.log('Server is listening on port ' + app.get('port') + '. Press CTRL-C to terminate.');
-});
