1ae17c9633ab7e28f456beb10965fccfb969afbf
Alireza Saberi <saberi.ar.eng@gmail.com>
2017-02-23 14:51:32 -0500

    feat: a basic home page is made for secret santa

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+/*
+* @Author: Ali
+* @Date:   2017-02-22 19:50:29
+* @Last Modified by:   Ali
+* @Last Modified time: 2017-02-22 21:26:23
+*/
+
+'use strict';
+var update = document.getElementById('update');
+
+update.addEventListener('click', function () {
+  // Send PUT Request here
+});
+
+var del = document.getElementById('delete');
+del.addEventListener('click', function () {
+  // Send DELETE Request here
+});
