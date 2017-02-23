a96e71a7cec4479d86638d1f6a90ab3402ab2b61
Alireza Saberi <saberi.ar.eng@gmail.com>
2017-02-22 19:24:29 -0500

    fix: handling scenario where use enter an empty name
    this input should not go into database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: Ali
 * @Date:   2017-02-22 11:00:40
 * @Last Modified by:   Ali
-* @Last Modified time: 2017-02-22 16:32:54
+* @Last Modified time: 2017-02-22 19:22:40
 */
 
 /******** Requiring libraries ********/
@@ -25,19 +25,33 @@ MongoClient.connect( config.mongoLaClient, (err, database) => {
         console.log('Server is listening on port ' + app.get('port') + '. Press CTRL-C to terminate.');
     });
 });
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/app'));
 
 /******** Routing ********/
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/app/index.html');
+    //res.sendFile(__dirname + '/app/index.html');
+    var cursor = db.collection('gifters').find().toArray(function(err, results) {
+         if (err) return console.log(err);
+         res.render('index.ejs', {gifters: results});
+    });
 });
 
 app.post('/register', (req, res) => {
-    req.body.match = '';
-    db.collection('gifters').save(req.body, (err, result) => {
-        if (err) return console.log(err);
-        console.log('saved to database');
+    if (req.body.name.length !== 0){
+        var newGifter = {};
+        newGifter.name = req.body.name.toLowerCase();
+        newGifter.spouse = req.body.spouse.toLowerCase();
+        newGifter.match = '';
+
+        db.collection('gifters').save(newGifter, (err, result) => {
+            if (err) return console.log(err);
+            console.log('saved to database');
+            res.redirect('/');
+            });
+    }else{
         res.redirect('/');
-    });
+    }
 
 });
 
